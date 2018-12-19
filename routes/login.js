f8ef84dc93297fa6ab08aad3bdf76ae3dfe1a973
lzjlxebr <tyyzmyd@gmail.com>
2018-12-19 21:23:58 +0800

    add user register and login

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,80 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'meetingroom',
-    database : 'cloudprint'
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'meetingroom'
 });
-connection.connect(function(err){
-    if(!err) {
+connection.connect(function (err) {
+    if (!err) {
         console.log("Database is connected ... nn");
     } else {
         console.log("Error connecting database ... nn");
     }
 });
+
+// user register
+exports.register = function (req, res) {
+    // console.log("req",req.body);
+    var today = new Date();
+    var users = {
+        "first_name": req.body.first_name,
+        "last_name": req.body.last_name,
+        "email": req.body.email,
+        "password": req.body.password,
+        "created": today,
+        "modified": today
+    }
+    connection.query('INSERT INTO users SET ?', users, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code": 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log('The solution is: ', results);
+            res.send({
+                "code": 200,
+                "success": "user registered sucessfully"
+            });
+        }
+    });
+}
+
+// user login
+exports.login = function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+        if (error) {
+            // console.log("error ocurred",error);
+            res.send({
+                "code": 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            // console.log('The solution is: ', results);
+            if (results.length > 0) {
+                if (results[0].password == password) {
+                    res.send({
+                        "code": 200,
+                        "success": "login sucessfull"
+                    });
+                }
+                else {
+                    res.send({
+                        "code": 204,
+                        "success": "Email and password does not match"
+                    });
+                }
+            }
+            else {
+                res.send({
+                    "code": 204,
+                    "success": "Email does not exits"
+                });
+            }
+        }
+    });
+}
